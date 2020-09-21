67217f70756a90978c41ec6cc1cbf6812ff5c2b3
Khajap <khajap.045@gmail.com>
2020-09-21 16:45:12 +0530

    krackone

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+ <head>
+<!--
+Project Title: Krackbot
+Designed & Developed By : Krackbot
+UI Design: Khaja
+Started Date: 9 Sept 2020
+-->
+  <title> Krackbot </title>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  <!-- bootstrap css  -->
+  <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+  <!--  animate.css-->
+  <link type="text/css" rel="stylesheet" href="css/animate.css">
+  <!--  style.css-->
+  <link type="text/css" rel="stylesheet" href="css/style.css">
+ </head>
+ <body>
+  <header class="menu">
+    <nav class="navbar navbar-expand-md navbar-dark">
+     <div class="container">
+  <!-- Brand -->
+  <a class="navbar-brand" href="#">
+   <img src="images/logo.png" alt="logo" title="krackbot" class="img-fluid logo">      
+  </a>
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Case Studies</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Packages</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+      <li class="nav-item d-none d-sm-block">
+        <a class="nav-link" href="#"><img src="images/bars.png" alt="" title="" class="img-fluid bars float-right"></a>
+      </li>
+    </ul>
+  </div>
+         </div>
+</nav>    
+        
+  </header>
+<!--     header closed   -->
+     
+<!-- banner section start-->
+    
+ <section>
+  <div class="container-fluid">
+   <div class="container">
+     <div class="row">
+        <div class="col-sm-6 pt-sm-5 mt-sm-5 pt-3 mt-3">
+        <div class="col-sm-12 css-typing">
+          <h1><b> Hi, there.. talk to me</b></h1>
+          <h1><b> I am Krackbot..</b></h1>
+           <h1> <b>Tell me about  Yourself..</b></h1>
+<!--
+            <h1 class="ml11">
+  <span class="text-wrapper">
+    <span class="line line1"></span>
+    <span class="letters"> <b><h1> Hi, there.. talk to me, </h1><br/><br/>
+        <h1> I am Krackbot..</h1>
+      Tell me about  Yourself..</b>
+      </span>
+  </span>
+</h1>
+-->
+          <form>
+           <div class="form-group py-3">
+            <input type="text" class="form-control form-control-lg bg-light col-sm-10" placeholder="Whats Your Name?"> 
+           </div>  
+          </form>
+          <h4><b> Wow you got a great game..</b></h4>
+          <h4><b>Cool.. so let me give a brief about who we are..</b></h4>
+          <h4><b>What we do</b></h4>
+          <h4><b>Let's go..</b></h4>
+         </div>
+         </div>
+         <div class="col-sm-6">
+          <div class="col-sm-11 offset-sm-1 pt-5">
+           <img src="images/peppa.png" alt="peppa pig" title="banner" class="img-fluid">   
+          </div>
+         </div>
+     </div>   
+   </div>   
+  </div>
+ </section>
+    
+     
+<!-- banner section end-->
+     
+     
+     
+     
+ <!--   script tags  -->
+ <script src="js/jquery.min.js"></script>
+ <script src="js/popper.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ 
+ </body>
+</html>
